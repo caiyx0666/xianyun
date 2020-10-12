6767ca94f41ef959331f5c825bebb59d462591ad
蔡英雄0666 <1601258964@qq.com>
2020-10-12 17:41:20 +0800

    引入登录和注册组件并应用到页面上

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -17,11 +17,25 @@
                         {{item}}
                     </span>
                 </el-row>
+
+                <!-- 登录功能组件 -->
+                <LoginForm v-if="currentTab == 0"/>
+
+                <!-- 注册功能组件 -->
+                <RegisterForm v-if="currentTab == 1"/>
+            </div>
+        </el-row>
     </div>
 </template>
 
 <script>
+import LoginForm from '@/components/user/LoginForm'
+import RegisterForm from '@/components/user/RegisterForm'
 export default {
+    components:{
+        LoginForm,
+        RegisterForm
+    },
     data(){
         return {
             currentTab: 0
