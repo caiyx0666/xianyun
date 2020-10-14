d5f3ff0eddd8401baf61e90b5ae19925404935f9
caiyx0666 <caiyx0666@163.com>
2020-10-14 12:13:56 +0800

    禁止服务端执行 需要本地 localStorage 数据持久化的代码

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,15 @@ export default {
      ** Plugins to load before mounting the App
      */
     plugins: [
-        '@/plugins/element-ui'
+        '@/plugins/element-ui',
+        // '@/plugins/localstorage'
+        // 引入插件的时候, 默认是字符串
+        // 如果需要额外的配置, 就变成对象
+        {
+            src: '@/plugins/localstorage',
+            // 指定这个插件不可以在服务端执行(只能等到浏览器当中再执行)
+            ssr: false
+        }
     ],
 
     /*
