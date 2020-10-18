1549e08945849ae3f153b35be9fa07109c368e6b
caiyx0666 <caiyx0666@163.com>
2020-10-18 11:22:45 +0800

    vuex 模拟死数据渲染历史记录

--- components/air/FlightsAside.vue
+++ components/air/FlightsAside.vue
@@ -23,7 +23,11 @@
 
         <div class="history">
             <h5>历史查询</h5>
-            <nuxt-link to="#">
+            <nuxt-link
+             to="#"
+             v-for="(item,index) in $store.state.history.historyList"
+             :key="index"
+             >
                 <el-row
                     type="flex"
                     justify="space-between"
@@ -31,8 +35,10 @@
                     class="history-item"
                 >
                     <div class="air-info">
-                        <div class="to-from">广州 - 上海</div>
-                        <p>2019-06-16</p>
+                        <div class="to-from">
+                            {{ item.departCity }} - {{ item.destCity }}
+                        </div>
+                        <p>{{ item.departDate }}</p>
                     </div>
                     <span>选择</span>
                 </el-row>
---
+++ store/history.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+    historyList: [
+        // 模拟一个历史记录
+        {
+            departCity: '南京',
+            departCode: "NKG",
+            destCity: '北京',
+            destCode: 'BJS',
+            departDate: '2020-10-20'
+        }
+    ]
+})
