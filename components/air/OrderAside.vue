ec2311a5a3cbb56b1e8329e569e969765f950fc0
caiyx0666 <caiyx0666@163.com>
2020-10-18 20:39:38 +0800

    补全侧边栏计算航班时间的计算属性

--- components/air/OrderAside.vue
+++ components/air/OrderAside.vue
@@ -51,7 +51,24 @@ export default {
     props: ['data'],
     computed:{
         rankTime(){
-            return 1
+            // 数据还未请求回来
+            if(!this.data.dep_time) return ''
+
+            // 转化为分钟
+            const dep = this.data.dep_time.split(':')
+            const arr = this.data.arr_time.split(':')
+            const depVal = dep[0] * 60 + +dep[1]
+            const arrVal = arr[0] * 60 + +arr[1]
+            // 到达时间相减得到分钟
+            let dis = arrVal - depVal
+
+            // 如果是第二天凌晨时间段，需要加24小时
+                console.log(dis/60);
+            if(dis < 0){
+                dis = arrVal + 24 * 60 - depVal
+            }
+            // 得到相差时间
+            return `${Math.floor(dis / 60)}时${dis % 60}分`
         }
     }
 }
