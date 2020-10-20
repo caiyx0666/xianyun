3751da1910cfd5c3055314d57ee0c2a16f8b946f
caiyx0666 <caiyx0666@163.com>
2020-10-20 10:54:21 +0800

    避免重复记录

--- store/history.js
+++ store/history.js
@@ -6,6 +6,22 @@ export const state = () => ({
 
 export const mutations = {
     addHistory(state, data) {
-        state.historyList.unshift(data)
+        // state.historyList.unshift(data)
+        // 先判断是否存在一样的数据，如果不存在在进行添加
+        let isUnique = true
+        state.historyList.forEach(history => {
+            if (
+                history.departCity == data.departCity,
+                history.departCode == data.departCode,
+                history.destCity == data.destCity,
+                history.destCode == data.destCode,
+                history.departDate == data.departDate
+            ) {
+                isUnique = false
+            }
+        })
+        if (isUnique) {
+            state.historyList.unshift(data)
+        }
     }
 }
