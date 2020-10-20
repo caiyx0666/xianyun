522ecfcf9af9f87692766441c6fa33dbc087d591
caiyx0666 <caiyx0666@163.com>
2020-10-20 17:23:48 +0800

    在订单页添加登录表单, 复用之前的组件

--- components/air/OrderForm.vue
+++ components/air/OrderForm.vue
@@ -103,6 +103,37 @@
                     </el-form-item>
                 </el-form>   
                 <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
+
+                <div class="container">
+                    <!-- 主要内容 -->
+                    <el-row
+                        type="flex"
+                        justify="center"
+                        align="middle"
+                        class="main"
+                    >
+                        <div class="form-wrapper">
+                            <!-- 表单头部tab -->
+                            <el-row type="flex" justify="center" class="tabs">
+                                <span
+                                    :class="{ active: currentTab === index }"
+                                    v-for="(item, index) in [`登录`, `注册`]"
+                                    :key="index"
+                                    @click="handleChangeTab(index)"
+                                >
+                                    {{ item }}
+                                </span>
+                            </el-row>
+
+                            <!-- 登录功能组件 -->
+                            <LoginForm v-if="currentTab == 0" />
+
+                            <!-- 注册功能组件 -->
+                            <RegisterForm v-if="currentTab == 1" />
+                        </div>
+                    </el-row>
+                </div>
+
             </div>
         </div>
         <div style="display: none">
@@ -118,6 +149,7 @@ export default {
     },
     data(){
         return {
+            currentTab: 0,
             contactRules: {
                 contactName: [
                     {
@@ -372,5 +404,47 @@ export default {
         display: block;
         width:250px;
         height:50px;
+    }
+
+     .container {
+        .main {
+            height: 100%;
+            margin: 0 auto;
+            position: relative;
+
+            .form-wrapper {
+                width: 400px;
+                margin: 0 auto;
+                background: #fff;
+                box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.1);
+                overflow: hidden;
+                
+                /deep/ .el-input {
+                    width: 100%;
+                }
+
+                .tabs {
+                    span {
+                        display: block;
+                        width: 50%;
+                        height: 50px;
+                        box-sizing: border-box;
+                        border-top: 2px #eee solid;
+                        background: #eee;
+                        line-height: 48px;
+                        text-align: center;
+                        cursor: pointer;
+                        color: #666;
+
+                        &.active {
+                            color: orange;
+                            border-top-color: orange;
+                            background: #fff;
+                            font-weight: bold;
+                        }
+                    }
+                }
+            }
+        }
     }
 </style>
