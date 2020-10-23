288873cee0541d4cd87245fc03afe5c8470b99e5
fancongcong <1799735862@qq.com>
2020-10-23 20:53:52 +0800

    ...

---
+++ components/post/postContent.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="con">
+    <!-- 顶部搜索部分 -->
+    <div class="search">
+      <el-input
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        class="input"
+      >
+      </el-input>
+      <div class="hotCity">
+        推荐:
+        <span class="adress"> 广州</span>
+        <span class="adress"> 上海</span>
+        <span class="adress"> 北京</span>
+      </div>
+    </div>
+    <!-- 推荐攻略部分 -->
+    <div class="title">
+      <div class="strate">推荐攻略</div>
+      <div class="button">
+        <el-button type="primary" icon="el-icon-edit">写游记</el-button>
+      </div>
+    </div>
+
+    <!-- 文章列表部分 -->
+    <div>
+      <PostArticle />
+    </div>
+  </div>
+</template>
+
+<script>
+import PostArticle from "@/components/post/postArticle";
+export default {
+  components: {
+    PostArticle,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.con {
+  width: 700px;
+
+  .search {
+    .input {
+      outline: none;
+      border: 3px solid orange;
+      margin-bottom: 2px;
+    }
+    .hotCity {
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      color: #555;
+      .adress {
+        margin: 0 6px;
+      }
+      .adress:hover {
+        color: orange;
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+  }
+  .title {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 20px;
+    .strate {
+      font-size: 18px;
+      color: orange;
+      height: 60px;
+      line-height: 60px;
+      border-bottom: 3px solid orange;
+    }
+  }
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <!-- 如果直接写, 那么在服务端不认识, 浏览器才认识
-        所以会造成前后渲染内容不一致, 报错 -->
-        <!-- 这里可以单独指定这一个标签要在客户端再渲染 -->
-        <client-only>
-            <VueEditor/>
-        </client-only>
-    </div>
+  <div class="container">
+    <PostAside />
+    <PostContent />
+  </div>
 </template>
 
 <script>
-    // import {VueEditor} from 'vue2-editor'
-    export default {
-        // components: {
-        //     VueEditor
-        // }
-    };
+export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  height: 3000px;
+  width: 1000px;
+  margin: 20px auto;
+}
 </style>
