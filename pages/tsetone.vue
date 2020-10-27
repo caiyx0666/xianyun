5d122965e396f487904ed506697e63637dbe3943
33 <821051557@qq.com>
2020-10-27 11:49:12 +0800

    发送文章axios请求

---
+++ pages/tsetone.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" v-model="content" />
+    <button @click="handleClick">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+      hotel: 268,
+      score: {
+        location: 4,
+        service: 4,
+        fancility: 3,
+        all: 3,
+      },
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$axios({
+        method: "post",
+        url: "/comments",
+        data: { content: this.content, hotel: "268", score: this.score },
+        headers: {
+          Authorization: "Bearer " + this.$store.state.user.userInfo.token,
+        },
+      }).then((res) => {
+        console.log(res.data);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
