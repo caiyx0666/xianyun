d693804063ee365ca2b50cbf0885b8ef1397a36b
caiyx0666 <caiyx0666@163.com>
2020-10-29 22:15:45 +0800

    ...

--- plugins/localstorage.js
+++ plugins/localstorage.js
@@ -9,18 +9,18 @@ import createPersistedState from 'vuex-persistedstate'
 //     如果只需要其中某些属性, 可以用结构的方式实现
 // }
 
-export default ({ store }) => {
+export default ( { store } ) => {
     // 类似window.onload
     // window.onNuxtReady 是页面上 nuxt 准备完毕的一个回调事件
-    window.onNuxtReady(() => {
+    window.onNuxtReady( () => {
         // 引入了插件的第三方包, 调用它实现数据持久化即可
         // 这个插件的用法是作者规定的
-        createPersistedState({
+        createPersistedState( {
             // 这个插件的目标是将 vuex 存放到本地储存
             // localStorage.setItem(key, this.$store)
             key: "store", // 读取本地存储的数据到store
-        })(store)
-    })
+        } )( store )
+    } )
 }
 
 // 所有原来 写在 main.js new Vue() 之前的逻辑
