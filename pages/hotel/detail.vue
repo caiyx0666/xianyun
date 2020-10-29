711892964d40f092dd40a61971f800d0bec3d2d3
33 <821051557@qq.com>
2020-10-29 11:25:04 +0800

    增加功能

--- pages/hotel/detail.vue
+++ pages/hotel/detail.vue
@@ -8,7 +8,27 @@
         <el-col>
           <div class="breadcrumb">
             <span style="font-weight: 700">您在这里：</span>
-            酒店 >
+            <div
+              @mouseenter="area = true"
+              @mouseleave="area = false"
+              style="display: inline-block"
+            >
+              酒店 >
+              <div class="area" v-show="area">
+                <div class="san"></div>
+                <div class="areaFirst">周边国家和地区</div>
+                <div class="middle">
+                  <div>韩国 <i>Korea</i></div>
+                  <div>朝鲜 <i>North Korea</i></div>
+                  <div>日本 <i>Japan</i></div>
+                  <div>俄罗斯 <i>Russia</i></div>
+                  <div>蒙古国 <i>Mongolia</i></div>
+                  <div>老挝 <i>Laos</i></div>
+                </div>
+
+                <div class="areaLast">>>更多国家和地区</div>
+              </div>
+            </div>
             <span @click="handleClick">{{ hotelList.real_city }}</span> >
             {{ hotelList.name }}
           </div>
@@ -214,6 +234,7 @@
 export default {
   data() {
     return {
+      area: false,
       show: false,
       showDian: false,
       showStart: true,
@@ -449,6 +470,7 @@ export default {
   // box-sizing: border-box;
 }
 .breadcrumb {
+  position: relative;
   padding: 20px 0;
   cursor: pointer;
 }
@@ -710,6 +732,51 @@ export default {
     width: 75px;
     font-size: 14px;
     color: #666666;
+  }
+}
+.area {
+  position: absolute;
+  left: 79px;
+  top: 46px;
+  background-color: #fff;
+  z-index: 999;
+  .san {
+    position: absolute;
+    left: 17px;
+    top: -11px;
+    border-style: solid;
+    border-width: 5px 5px 5px 5px;
+    border-color: transparent transparent #ccc transparent;
+    width: 0px;
+    height: 0px;
+    transform: rotate(-360deg);
+  }
+  cursor: pointer;
+  width: 150px;
+  height: 200px;
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  font-size: 12px;
+  .areaFirst {
+    font-weight: 700;
+    padding: 20px 0 0 10px;
+  }
+  i {
+    color: #cccccc;
+  }
+  .areaLast {
+    color: #f90;
+    padding: 3px 0 0 30px;
+  }
+  .middle {
+    padding: 0 10px;
+    > div {
+      padding: 3px;
+    }
+    :hover {
+      background-color: #ff99003d;
+      color: #f90;
+    }
   }
 }
 </style>
