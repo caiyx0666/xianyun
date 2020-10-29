b8db828175b4fd8d76566e01a5be45515b5f0fed
caiyx0666 <caiyx0666@163.com>
2020-10-29 22:08:19 +0800

    下载相应的包

---
+++ .nuxt/components/index.js
@@ -0,0 +1,61 @@
+export { default as PageFooter } from '../..\\components\\PageFooter.vue'
+export { default as PageHeader } from '../..\\components\\PageHeader.vue'
+export { default as FlightsAside } from '../..\\components\\air\\FlightsAside.vue'
+export { default as FlightsFilter } from '../..\\components\\air\\FlightsFilter.vue'
+export { default as FlightsItem } from '../..\\components\\air\\FlightsItem.vue'
+export { default as FlightsListHead } from '../..\\components\\air\\FlightsListHead.vue'
+export { default as OrderAside } from '../..\\components\\air\\OrderAside.vue'
+export { default as OrderForm } from '../..\\components\\air\\OrderForm.vue'
+export { default as SearchForm } from '../..\\components\\air\\SearchForm.vue'
+export { default as Booking } from '../..\\components\\hotel\\Booking.vue'
+export { default as Comment } from '../..\\components\\hotel\\comment.vue'
+export { default as Evaluate } from '../..\\components\\hotel\\Evaluate.vue'
+export { default as Evaluate1 } from '../..\\components\\hotel\\Evaluate1.vue'
+export { default as HotelFilter } from '../..\\components\\hotel\\HotelFilter.vue'
+export { default as HotelList } from '../..\\components\\hotel\\HotelList.vue'
+export { default as HotelQuery } from '../..\\components\\hotel\\HotelQuery.vue'
+export { default as Info } from '../..\\components\\hotel\\Info.vue'
+export { default as Like } from '../..\\components\\hotel\\Like.vue'
+export { default as Map } from '../..\\components\\hotel\\Map.vue'
+export { default as Nav } from '../..\\components\\hotel\\Nav.vue'
+export { default as ArticlesList } from '../..\\components\\post\\ArticlesList.vue'
+export { default as Main } from '../..\\components\\post\\Main.vue'
+export { default as Parent } from '../..\\components\\post\\Parent.vue'
+export { default as PostArticle } from '../..\\components\\post\\postArticle.vue'
+export { default as PostAside } from '../..\\components\\post\\postAside.vue'
+export { default as PostContent } from '../..\\components\\post\\postContent.vue'
+export { default as LoginForm } from '../..\\components\\user\\LoginForm.vue'
+export { default as RegisterForm } from '../..\\components\\user\\RegisterForm.vue'
+export { default as CommentMain } from '../..\\components\\test\\CommentMain.vue'
+export { default as CommentParent } from '../..\\components\\test\\CommentParent.vue'
+
+export const LazyPageFooter = import('../..\\components\\PageFooter.vue' /* webpackChunkName: "components_PageFooter" */).then(c => c.default || c)
+export const LazyPageHeader = import('../..\\components\\PageHeader.vue' /* webpackChunkName: "components_PageHeader" */).then(c => c.default || c)
+export const LazyFlightsAside = import('../..\\components\\air\\FlightsAside.vue' /* webpackChunkName: "components_air/FlightsAside" */).then(c => c.default || c)
+export const LazyFlightsFilter = import('../..\\components\\air\\FlightsFilter.vue' /* webpackChunkName: "components_air/FlightsFilter" */).then(c => c.default || c)
+export const LazyFlightsItem = import('../..\\components\\air\\FlightsItem.vue' /* webpackChunkName: "components_air/FlightsItem" */).then(c => c.default || c)
+export const LazyFlightsListHead = import('../..\\components\\air\\FlightsListHead.vue' /* webpackChunkName: "components_air/FlightsListHead" */).then(c => c.default || c)
+export const LazyOrderAside = import('../..\\components\\air\\OrderAside.vue' /* webpackChunkName: "components_air/OrderAside" */).then(c => c.default || c)
+export const LazyOrderForm = import('../..\\components\\air\\OrderForm.vue' /* webpackChunkName: "components_air/OrderForm" */).then(c => c.default || c)
+export const LazySearchForm = import('../..\\components\\air\\SearchForm.vue' /* webpackChunkName: "components_air/SearchForm" */).then(c => c.default || c)
+export const LazyBooking = import('../..\\components\\hotel\\Booking.vue' /* webpackChunkName: "components_hotel/Booking" */).then(c => c.default || c)
+export const LazyComment = import('../..\\components\\hotel\\comment.vue' /* webpackChunkName: "components_hotel/comment" */).then(c => c.default || c)
+export const LazyEvaluate = import('../..\\components\\hotel\\Evaluate.vue' /* webpackChunkName: "components_hotel/Evaluate" */).then(c => c.default || c)
+export const LazyEvaluate1 = import('../..\\components\\hotel\\Evaluate1.vue' /* webpackChunkName: "components_hotel/Evaluate1" */).then(c => c.default || c)
+export const LazyHotelFilter = import('../..\\components\\hotel\\HotelFilter.vue' /* webpackChunkName: "components_hotel/HotelFilter" */).then(c => c.default || c)
+export const LazyHotelList = import('../..\\components\\hotel\\HotelList.vue' /* webpackChunkName: "components_hotel/HotelList" */).then(c => c.default || c)
+export const LazyHotelQuery = import('../..\\components\\hotel\\HotelQuery.vue' /* webpackChunkName: "components_hotel/HotelQuery" */).then(c => c.default || c)
+export const LazyInfo = import('../..\\components\\hotel\\Info.vue' /* webpackChunkName: "components_hotel/Info" */).then(c => c.default || c)
+export const LazyLike = import('../..\\components\\hotel\\Like.vue' /* webpackChunkName: "components_hotel/Like" */).then(c => c.default || c)
+export const LazyMap = import('../..\\components\\hotel\\Map.vue' /* webpackChunkName: "components_hotel/Map" */).then(c => c.default || c)
+export const LazyNav = import('../..\\components\\hotel\\Nav.vue' /* webpackChunkName: "components_hotel/Nav" */).then(c => c.default || c)
+export const LazyArticlesList = import('../..\\components\\post\\ArticlesList.vue' /* webpackChunkName: "components_post/ArticlesList" */).then(c => c.default || c)
+export const LazyMain = import('../..\\components\\post\\Main.vue' /* webpackChunkName: "components_post/Main" */).then(c => c.default || c)
+export const LazyParent = import('../..\\components\\post\\Parent.vue' /* webpackChunkName: "components_post/Parent" */).then(c => c.default || c)
+export const LazyPostArticle = import('../..\\components\\post\\postArticle.vue' /* webpackChunkName: "components_post/postArticle" */).then(c => c.default || c)
+export const LazyPostAside = import('../..\\components\\post\\postAside.vue' /* webpackChunkName: "components_post/postAside" */).then(c => c.default || c)
+export const LazyPostContent = import('../..\\components\\post\\postContent.vue' /* webpackChunkName: "components_post/postContent" */).then(c => c.default || c)
+export const LazyLoginForm = import('../..\\components\\user\\LoginForm.vue' /* webpackChunkName: "components_user/LoginForm" */).then(c => c.default || c)
+export const LazyRegisterForm = import('../..\\components\\user\\RegisterForm.vue' /* webpackChunkName: "components_user/RegisterForm" */).then(c => c.default || c)
+export const LazyCommentMain = import('../..\\components\\test\\CommentMain.vue' /* webpackChunkName: "components_test/CommentMain" */).then(c => c.default || c)
+export const LazyCommentParent = import('../..\\components\\test\\CommentParent.vue' /* webpackChunkName: "components_test/CommentParent" */).then(c => c.default || c)
---
+++ .nuxt/router.js
@@ -0,0 +1,105 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _7714db00 = () => interopDefault(import('..\\pages\\air\\index.vue' /* webpackChunkName: "pages/air/index" */))
+const _70417bd6 = () => interopDefault(import('..\\pages\\hotel\\index.vue' /* webpackChunkName: "pages/hotel/index" */))
+const _70cfd72a = () => interopDefault(import('..\\pages\\post\\index.vue' /* webpackChunkName: "pages/post/index" */))
+const _4705e56c = () => interopDefault(import('..\\pages\\test.vue' /* webpackChunkName: "pages/test" */))
+const _1c892bb0 = () => interopDefault(import('..\\pages\\tsetone.vue' /* webpackChunkName: "pages/tsetone" */))
+const _c1fb29c2 = () => interopDefault(import('..\\pages\\user\\index.vue' /* webpackChunkName: "pages/user/index" */))
+const _9810749e = () => interopDefault(import('..\\pages\\air\\flights.vue' /* webpackChunkName: "pages/air/flights" */))
+const _457f5a08 = () => interopDefault(import('..\\pages\\air\\order.vue' /* webpackChunkName: "pages/air/order" */))
+const _44114cd6 = () => interopDefault(import('..\\pages\\air\\pay.vue' /* webpackChunkName: "pages/air/pay" */))
+const _30410e7d = () => interopDefault(import('..\\pages\\hotel\\detail.vue' /* webpackChunkName: "pages/hotel/detail" */))
+const _c846cd64 = () => interopDefault(import('..\\pages\\post\\article.vue' /* webpackChunkName: "pages/post/article" */))
+const _417e1da9 = () => interopDefault(import('..\\pages\\post\\detail.vue' /* webpackChunkName: "pages/post/detail" */))
+const _68aa0e36 = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages/user/login" */))
+const _0b888ffa = () => interopDefault(import('..\\pages\\user\\personal.vue' /* webpackChunkName: "pages/user/personal" */))
+const _680b516c = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/air",
+    component: _7714db00,
+    name: "air"
+  }, {
+    path: "/hotel",
+    component: _70417bd6,
+    name: "hotel"
+  }, {
+    path: "/post",
+    component: _70cfd72a,
+    name: "post"
+  }, {
+    path: "/test",
+    component: _4705e56c,
+    name: "test"
+  }, {
+    path: "/tsetone",
+    component: _1c892bb0,
+    name: "tsetone"
+  }, {
+    path: "/user",
+    component: _c1fb29c2,
+    name: "user"
+  }, {
+    path: "/air/flights",
+    component: _9810749e,
+    name: "air-flights"
+  }, {
+    path: "/air/order",
+    component: _457f5a08,
+    name: "air-order"
+  }, {
+    path: "/air/pay",
+    component: _44114cd6,
+    name: "air-pay"
+  }, {
+    path: "/hotel/detail",
+    component: _30410e7d,
+    name: "hotel-detail"
+  }, {
+    path: "/post/article",
+    component: _c846cd64,
+    name: "post-article"
+  }, {
+    path: "/post/detail",
+    component: _417e1da9,
+    name: "post-detail"
+  }, {
+    path: "/user/login",
+    component: _68aa0e36,
+    name: "user-login"
+  }, {
+    path: "/user/personal",
+    component: _0b888ffa,
+    name: "user-personal"
+  }, {
+    path: "/",
+    component: _680b516c,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  return new Router(routerOptions)
+}
